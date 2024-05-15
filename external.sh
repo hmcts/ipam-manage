dd9a5f12c79827ac05e8a5a1f10128330512f898
Chirag Pareek <chirag.pareek@justice.gov.uk>
2024-05-15 16:16:55 +0100

    refactor pipelines and script to allow auto sync of vnets

--- external.sh
+++ external.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-
 function add_external_cidr() {
     space=$1
     block=$2
     startswith=$3
     output=$4
-    vnet_file=$5
+    vnet_data="$5"
 
     # filtering the effective routes IPs with the appropriate CIDR starting with
     output_filter=$(echo "$output" | jq '[.[] | select(.addressPrefix[0] | startswith("'"$startswith"'"))]')
@@ -22,11 +21,15 @@ function add_external_cidr() {
         # copy data to json file
         echo "$converted_output" > $filename
         
+        resource_ids=$(echo "$vnet_data" | jq -r '.[]')
         
-        resource_ids=$(jq -r '.[]' $vnet_file)
-        
-        cidr_list=$(az network vnet show --ids $resource_ids --query "[].addressSpace.addressPrefixes")
-        
+        if [[ -z $resource_ids ]]; then
+            cidr_list=""
+        else
+            cidr_list=$(az network vnet show --ids $resource_ids --query "[].addressSpace.addressPrefixes")            
+        fi
+
+        echo $cidr_list
         # call the python script to find the overlapping CIDRs and remove them from json file
         python3 findoverlapping.py $filename "$cidr_list"
         
@@ -48,45 +51,50 @@ function run_env() {
     name=$2
     subscription=$3
     rg=$4
-    vnet_file=$5
 
     az account set --subscription $subscription
     # getting all the effective routes from the NIC
     output=$(az network nic show-effective-route-table --name $name --resource-group $rg --query "value[?nextHopType=='VirtualNetworkGateway' && !contains(addressPrefix, '10.0.0.0/8')]")
     
+    vnet_data=""
+
     block_10="$space""_10"
+    vnet_data=$(./function.sh get '' "/api/spaces/$space/blocks/$block_10/available")
     startswith_10="10."
-    add_external_cidr "$space" "$block_10" "$startswith_10" "$output" "$vnet_file"
+    add_external_cidr "$space" "$block_10" "$startswith_10" "$output" "$vnet_data"
 
     block_172="$space""_172"
+    vnet_data=$(./function.sh get '' "/api/spaces/$space/blocks/$block_172/available")
     startswith_172="172."
-    add_external_cidr "$space" "$block_172" "$startswith_172" "$output" "$vnet_file"
+    add_external_cidr "$space" "$block_172" "$startswith_172" "$output" "$vnet_data"
 
     block_163="$space""_163"
+    vnet_data=$(./function.sh get '' "/api/spaces/$space/blocks/$block_163/available")
     startswith_163="163."
-    add_external_cidr "$space" "$block_163" "$startswith_163" "$output" "$vnet_file"
+    add_external_cidr "$space" "$block_163" "$startswith_163" "$output" "$vnet_data"
 
 
     block_198="$space""_198"
+    vnet_data=$(./function.sh get '' "/api/spaces/$space/blocks/$block_198/available")
     startswith_198="198."
-    add_external_cidr "$space" "$block_198" "$startswith_198" "$output" "$vnet_file"
+    add_external_cidr "$space" "$block_198" "$startswith_198" "$output" "$vnet_data"
 
     block_192="$space""_192"
+    vnet_data=$(./function.sh get '' "/api/spaces/$space/blocks/$block_192/available")
     startswith_192="192."
-    add_external_cidr "$space" "$block_192" "$startswith_192" "$output" "$vnet_file"
+    add_external_cidr "$space" "$block_192" "$startswith_192" "$output" "$vnet_data"
 }
 space="nonprod"
 name="hmcts-hub-nonprodi-nic-transit-public-0"
 subscription="HMCTS-HUB-NONPROD-INTSVC"
 rg="hmcts-hub-nonprodi"
-vnet_file="nonprod_vnets.json"
 
-run_env "$space" "$name" "$subscription" "$rg" "$vnet_file"
+run_env "$space" "$name" "$subscription" "$rg"
 
 
 space="prod"
 name="hmcts-hub-prod-int-nic-transit-public-0"
 subscription="HMCTS-HUB-PROD-INTSVC"
 rg="hmcts-hub-prod-int"
-vnet_file="prod_vnets.json"
-run_env "$space" "$name" "$subscription" "$rg" "$vnet_file"
+
+run_env "$space" "$name" "$subscription" "$rg"
--- sync-vnets.sh
+++ sync-vnets.sh
@@ -1,46 +1,11 @@
 #!/bin/bash
 
+function process_prefixes() {
+        space=$1
+        prefixes="$2"
+        block_name=$3
 
-status=$(./function.sh get '' "/api/status")
-
-function block_name() {
-    space=$1
-    prefix=$2
-
-    if [[ "$prefix" == 10* ]]; then
-        echo "$space""_10"
-    elif [[ "$prefix" == 172* ]]; then
-        echo "$space""_172"
-    elif [[ "$prefix" == 163* ]]; then
-        echo "$space""_163"
-    elif [[ "$prefix" == 198* ]]; then
-        echo "$space""_198"
-    elif [[ "$prefix" == 192* ]]; then
-        echo "$space""_192"
-    else
-        echo "Unknown Prefix : $prefix"
-    fi
-
-}
-
-if [[ -z $status ]]; then
-    echo "Cannot connect to IPAM apis!"
-else
-    json_data=$(./function.sh get '' "/api/spaces/sbox/blocks/sbox_10/available?expand=true")
-
-    echo "$json_data" | jq -c '.[]' | while read -r vnet; do
-        name=$(echo "$vnet" | jq -r '.name')
-        id=$(echo "$vnet" | jq -r '.id')
-        prefixes=$(echo "$vnet" | jq -r '.prefixes[]')
-
-        # Check for 'sbox' or 'sandbox' in the name
-        if [[ "$name" =~ sbox|sandbox ]]; then
-            echo "Sandbox environment detected: $name"
-            space="sbox"
-
-            for prefix in $prefixes; do
-                echo "Working with prefix: $prefix"
-                block_name=$(block_name $space $prefix)
+        for prefix in $prefixes; do
                 
                 existing_associated_vnets=$(./function.sh get '' "/api/spaces/$space/blocks/$block_name/networks")
                 
@@ -48,26 +13,79 @@ else
                     echo "$id  already associated"
                 else 
                     echo "Associate: $id  "
-                fi
-                # call the function to associate vnet with block, it will only associate if its not overlapping
-                # ./function.sh post '{"id": "'"$id"'", "active": true}' "/api/spaces/$space/blocks/$block_name/networks"
-                    
+                    # call the function to associate vnet with block, it will only associate if its not overlapping
+            
+                    ./function.sh post '{"id": "'"$id"'", "active": true}' "/api/spaces/$space/blocks/$block_name/networks"
+                fi                    
                 echo -e "\n"
-            done
-            echo -e "\n"
+        done
+}
 
+function call_json_config() {
+    space=$1
+    subscriptions=$2
+
+    block="$space""_10"
+    json_config $space $block "$subscriptions"
+    
+    block="$space""_172"
+    json_config $space $block "$subscriptions"
+
+    block="$space""_163"
+    json_config $space $block "$subscriptions"
+
+    block="$space""_198"
+    json_config $space $block "$subscriptions"
+
+    block="$space""_192"
+    json_config $space $block "$subscriptions"
+
+
+}
+
+function json_config() {
+    space=$1
+    block=$2
+    subscriptions=$3
+    
+    json_data=$(./function.sh get '' "/api/spaces/$space/blocks/$block/available?expand=true")
+
+    echo "$json_data" | jq -c '.[]' | while read -r vnet; do
+        name=$(echo "$vnet" | jq -r '.name' | tr '[:upper:]' '[:lower:]')
+        id=$(echo "$vnet" | jq -r '.id')
+        prefixes=$(echo "$vnet" | jq -r '.prefixes[]')
+        subscription_id=$(echo "$vnet" | jq -r '.subscription_id')
+
+        subscription_name=$(echo "$subscriptions" | jq -r '.[] | select(.id == "'$subscription_id'") | .name' | tr '[:upper:]' '[:lower:]')
+
+        # Check for 'sbox' or 'sandbox' in the name
+        if [[ "$space" == sbox ]] && { [[ "$name" =~ (sbox|sandbox) ]] || [[ "$subscription_name" =~ (sbox|sandbox) ]]; }; then
+            echo "Sandbox environment detected: $name"
+            process_prefixes $space "$prefixes" $block
         # Check for 'prod' in the name
-        elif [[ "$name" =~ prod|ptl ]] && [[ ! "$name" =~ nonprod ]]; then
+        elif [[ "$space" == prod ]] && { [[ "$name" =~ (prod|ptl) ]] || [[ "$subscription_name" =~ (prod|ptl) ]]; } && [[ ! "$name" =~ nonprod ]] && [[ ! "$name" =~ ptlsbox ]]; then
             echo "Production environment detected: $name"
-            echo -e "\n"
-        # Default case
-        else
-            echo "Other environment detected: $name"
-            echo -e "\n"
-        fi
-
-        
-  
+            process_prefixes $space "$prefixes" $block
+        # Check for 'nonprod' in the name and ignore anything with sbox,prod and ptl
+        elif [[ "$space" == nonprod ]] && [[ ! "$name" =~ (sbox|sandbox|ptl) ]] && [[ ! "$subscription_name" =~ (sbox|sandbox|ptl) ]]; then
+            echo "NonProd environment detected: $name"
+            process_prefixes $space "$prefixes" $block
+        fi  
     done
+}
 
+status=$(./function.sh get '' "/api/status")
+
+
+if [[ -z $status ]]; then
+    echo "Cannot connect to IPAM apis!"
+else
+
+    subscriptions=$(az account list --query "[].{name:name,id:id}")
+    call_json_config "sbox" "$subscriptions"
+
+    call_json_config "prod" "$subscriptions"
+
+    call_json_config "nonprod" "$subscriptions"
+    
 fi
